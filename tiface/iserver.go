7696eb74347b53859b27f098a0f040211f36f7dd
HOU-SZ <1258271171@qq.com>
2022-07-16 16:28:31 +0800

    V0.1 - Basic Server

---
+++ tiface/iserver.go
@@ -0,0 +1,10 @@
+package tiface
+
+type IServer interface {
+	//启动服务器方法
+	Start()
+	//停止服务器方法
+	Stop()
+	//开启业务服务方法
+	Serve()
+}
---
+++ tnet/server_test.go
@@ -0,0 +1,43 @@
+package tnet
+
+import (
+	"fmt"
+	"net"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestServer(t *testing.T) {
+
+	//1 创建一个server 句柄 s
+	s := NewServer("[zinx V0.1]")
+
+	//2 开启服务
+	go s.Serve()
+
+	fmt.Println("Test ... start")
+	//3秒之后发起测试请求，给服务端开启服务的机会
+	time.Sleep(3 * time.Second)
+
+	conn, err := net.Dial("tcp", "127.0.0.1:7777")
+	if err != nil {
+		fmt.Println("client start err, exit!")
+		return
+	}
+
+	for i := 0; i < 5; i++ {
+		_, err := conn.Write([]byte("hahaha"))
+		require.NoError(t, err)
+
+		buf := make([]byte, 512)
+		cnt, err := conn.Read(buf)
+		require.NoError(t, err)
+		require.NotNil(t, buf)
+		require.Equal(t, "hahaha", string(buf[:cnt]))
+		fmt.Printf(" server call back : %s, cnt = %d\n", buf, cnt)
+
+		time.Sleep(1 * time.Second)
+	}
+}
