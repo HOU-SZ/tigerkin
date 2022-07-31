75fc7021ca0043f4af43a5760a533a392306d2f4
HOU-SZ <1258271171@qq.com>
2022-08-01 01:20:12 +0800

    Add a demo app mmo_game based on the Tigerkin framework, and add the AOI (Area Of Interest) module

---
+++ demo_app/mmo_game/core/aoi_test.go
@@ -0,0 +1,30 @@
+package core
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestNewAOIManager(t *testing.T) {
+	// 初始化AOIanager
+	aoiMgr := NewAOIManager(100, 300, 4, 200, 450, 5)
+
+	// 打印AOIManager
+	fmt.Println(aoiMgr)
+}
+
+func TestAOIManagerSuroundGridsByGid(t *testing.T) {
+	aoiMgr := NewAOIManager(0, 250, 5, 0, 250, 5)
+
+	for k, _ := range aoiMgr.grids {
+		// 得到当前格子周边的九宫格
+		grids := aoiMgr.GetSurroundGridsByGid(k)
+		// 得到九宫格所有的IDs
+		fmt.Println("gid : ", k, " grids len = ", len(grids))
+		gIDs := make([]int, 0, len(grids))
+		for _, grid := range grids {
+			gIDs = append(gIDs, grid.GID)
+		}
+		fmt.Printf("grid ID: %d, surrounding grid IDs are %v\n", k, gIDs)
+	}
+}
